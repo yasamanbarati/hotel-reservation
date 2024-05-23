ef3a462ac5db225d88827721d9ddaa49c5e7fd7d
yasamanbarati <yasamanbarati81@gmail.com>
2024-05-23 21:28:11 +0330

    Development theme structure

--- src/setup/theme/palette.ts
+++ src/setup/theme/palette.ts
@@ -1,4 +1,15 @@
 export const palette = {
+  primary: {
+    main : '#336699',
+    light: '#85a3c2'
+  },
+  secondary: {
+    main : '#997433',
+    light: '#c2ac85'
+  },
+  accent: {
+    main: '#994133'
+  },
   neutral: {
     main: '#fff',
     contrastText: '#444444',
@@ -13,10 +24,16 @@ declare module '@mui/material/styles' {
   interface Palette {
     neutral: Palette['primary']
     dark: Palette['primary']
+    primary: Palette['primary']
+    secondary: Palette['primary']
+    accent: Palette['primary']
   }
   interface PaletteOptions {
     neutral?: PaletteOptions['primary']
     dark?: PaletteOptions['primary']
+    primary?: PaletteOptions['primary']
+    secondary?: PaletteOptions['primary']
+    accent?: PaletteOptions['primary']
   }
 }
 declare module '@mui/material/Button' {
