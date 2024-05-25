2abfaa5f419848419e118d9f1b30be0897122c43
yasamanbarati <yasamanbarati81@gmail.com>
2024-05-25 23:01:46 +0330

    Improvement code structure(introduction section)

--- src/scenes/pages/components/home/introduction_section/index.tsx
+++ src/scenes/pages/components/home/introduction_section/index.tsx
@@ -1,15 +1,38 @@
-import { Grid, ImageList, ImageListItem, Typography, styled } from "@mui/material"
+import {
+  Grid,
+  ImageList,
+  ImageListItem,
+  Typography,
+  styled,
+} from "@mui/material";
 import { specialSaleData } from "services/server/mock";
 
-const DescriptionBox = styled('div')(({theme})=>({
+const DescriptionBox = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.primary.light,
-  borderRadius: '16px',
-  boxShadow :`0 4px 15px -5px ${theme.palette.primary.light}`,
-  padding: '16px',
-  display:'grid',
-  gap: '8px',
-  width: '280px',
-}))
+  borderRadius: "16px",
+  boxShadow: `0 4px 15px -5px ${theme.palette.primary.light}`,
+  padding: "16px",
+  display: "grid",
+  gap: "8px",
+  width: "280px",
+  [theme.breakpoints.down("md")]: {
+    width: "100%",
+  },
+}));
+
+const ImageListBox = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  [theme.breakpoints.up("md")]: {
+    width: "100%",
+    height: "100%",
+  },
+  [theme.breakpoints.up("xs")]: {
+    width: "100%",
+    height: "50%",
+  },
+}));
 
 function srcset(image: string, size: number, rows = 1, cols = 1) {
   return {
@@ -21,65 +44,69 @@ function srcset(image: string, size: number, rows = 1, cols = 1) {
 }
 
 export const Introduction = () => {
-    return (
-        <Grid
-          my={8}
-          container
-          height="100%"
-          minHeight="inherit"
-          spacing={0}
-          justifyContent="space-between"
-          alignItems="center"
-          flexDirection={{ xs: "column-reverse", md: "row-reverse" }}
+  return (
+    <Grid
+      my={4}
+      container
+      height="100%"
+      minHeight="inherit"
+      spacing={0}
+      justifyContent="space-between"
+      alignItems="center"
+      flexDirection={{ xs: "column-reverse", md: "row-reverse" }}
+    >
+      <ImageListBox item xs={12} md={6}>
+        <ImageList
+          sx={{
+            height: 576,
+            width: "100%",
+            transform: "translateZ(0)",
+            overflow: "hidden",
+          }}
+          rowHeight={276}
+          cols={4}
+          gap={8}
+        >
+          {specialSaleData.map((item) => (
+            <ImageListItem
+              key={item.img}
+              cols={item.cols || 1}
+              rows={item.rows || 1}
+            >
+              <img
+                {...srcset(item.img, 121, item.rows, item.cols)}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </ImageListBox>
+
+      <Grid item xs={12} md={6} height={{ md: "100%", xs: "50%" }}>
+        <Typography component="h2" variant="h3">
+          Parsian Azadi Hotel, a leader in providing modern hotel services
+        </Typography>
+        <Typography my={2} component="p" variant="body1">
+          Parsian Azadi or Hyatt Hotel has been operating since 1370 under the
+          supervision of Parsian Hotels Group. This hotel, which is one of the
+          largest and tallest five-star international hotels in Iran, was
+          completely renovated in 2011 at a cost of 80 billion Tomans.
+        </Typography>
+
+        <DescriptionBox>
+          <Typography
+            component="h6"
+            variant="h6"
+            color="primary"
+            fontWeight="700"
           >
-            <Grid
-              item
-              xs={12}
-              md={6}
-              height={{ md: '100%', xs: '50%' }}
-              display="flex"
-              alignItems="center"
-              justifyContent='flex-end'
-            >
-              <ImageList
-                sx={{
-                    height: 576,
-                    transform: 'translateZ(0)',
-                    overflow:'hidden',
-                  }}
-                rowHeight={276}
-                cols={4}
-                gap={8}>
-                {specialSaleData.map((item) => (
-                  <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                    <img
-                      {...srcset(item.img, 121, item.rows, item.cols)}
-                      loading="lazy"
-                    />
-                  </ImageListItem>
-                    ))}
-              </ImageList>
-            </Grid>
-
-            <Grid
-              item
-              xs={12}
-              md={6}
-              height={{ md: '100%', xs: '50%' }}
-            >
-              <Typography component='h2' variant="h3">Parsian Azadi Hotel, a leader in providing modern hotel services</Typography>
-              <Typography my={2} component='p' variant="body1">Parsian Azadi or Hyatt Hotel has been operating since 1370 under the supervision of Parsian Hotels Group. This hotel, which is one of the largest and tallest five-star international hotels in Iran, was completely renovated in 2011 at a cost of 80 billion Tomans.</Typography>
-
-              <DescriptionBox>
-                <Typography component='h6' variant="h6" color='primary'>
-                  475 residence units
-                </Typography>
-                <Typography component='p' variant="body1">
-                    With a variety of views, layouts and facilities
-                </Typography>
-              </DescriptionBox>
-
-            </Grid>
-        </Grid>
-      )
-}
+            475 residence units
+          </Typography>
+          <Typography component="p" variant="body1">
+            With a variety of views, layouts and facilities
+          </Typography>
+        </DescriptionBox>
+      </Grid>
+    </Grid>
+  );
+};
